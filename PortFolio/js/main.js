9b5e1e3197fba7cfeafa8fb14c66d647a010e2a0
SURAJ_P <103306408+SURAJ-R-P@users.noreply.github.com>
2022-06-08 22:06:52 +0530

    Add files via upload

---
+++ PortFolio/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+    "use strict";
+    // scroll
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.navbar').fadeIn('slow').css('display', 'flex');
+        } else {
+            $('.navbar').fadeOut('slow').css('display', 'none');
+        }
+    });
+
+    // typed
+    if ($('.typed-text-output').length == 1) {
+        var typed_strings = $('.typed-text').text();
+        var typed = new Typed('.typed-text-output', {
+            strings: typed_strings.split(', '),
+            typeSpeed: 100,
+            backSpeed: 20,
+            smartBackspace: false,
+            loop: true
+        });
+
+
+    }
+})(jQuery);
